64d4da876bb98af25c5ba9f23fbdaa43499297f9
Maciek <62883158+maacnt@users.noreply.github.com>
2025-08-24 00:52:42 +0200

    Update shared.py
    whoopsie, forgot to remove it from my debug

--- shared.py
+++ shared.py
@@ -6,7 +6,7 @@ import re
 from data_from_link import data_from_link
 
 
-link_to_look = "https://www.otomoto.pl/osobowe/bmw/seria-3/seg-coupe--seg-sedan/od-1998?search%5Bfilter_enum_generation%5D%5B0%5D=gen-e46-1998-2007&search%5Bfilter_enum_generation%5D%5B1%5D=gen-e90-2005-2012&search%5Bfilter_float_price%3Ato%5D=30000&search%5Bfilter_float_year%3Ato%5D=2013&search%5Border%5D=created_at%3Adesc"
+link_to_look = ""
 
 if link_to_look == "":
     link_to_look = input("Paste in the link with already applied filters to look for a car (the link can be set as static under the 'shared.py' file in line 9)\n")
